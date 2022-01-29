051d77e7c924075d3a720753464a9441732ec136
Zectar7 <taiga2002@outlook.jp>
2022-01-29 14:09:28 +0900

    change font, margin

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,8 @@
 ﻿export const Header = () => {
   return (
-      <header style={{fontSize: "30px", marginBottom: "5px"}}>
+      <header>
           文字数カウンター
-          <div style={{fontSize: "15px"}}>「クリップボードの内容を貼り付ける」ボタンの使用は、ユーザー（ブラウザ）の許可が必要です。</div>
+          <div id="head-desc">「クリップボードの内容を貼り付ける」ボタンの使用は、ユーザー（ブラウザ）の許可が必要です。</div>
       </header>
   );
 };
